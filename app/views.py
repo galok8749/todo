661e4fd7f74e36bcd497068fe2e55ebd15e101c4
alok gupta <galok8749@gmail.com>
2021-03-08 14:33:09 +0530

    block content applied

--- app/views.py
+++ app/views.py
@@ -1,13 +1,40 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login as loginUser
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 # Create your views here.
 
 def home(request):
     return render(request, 'app/index.html')
 
+
+
 def login(request):
-    return render(request, 'app/login.html')
+    if request.method == 'GET':
+        form = AuthenticationForm()
+        context = {
+            'form':form
+        }
+        return render(request, 'app/login.html', context=context)
+    else:
+        form = AuthenticationForm(data=request.POST)
+
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+             
+            if user is not None:
+                loginUser(request, user)
+                return redirect('home')
+        else:
+            form = AuthenticationForm()
+            context = {
+                'form':form
+            }
+            return render(request, 'app/login.html', context=context)
+
+
 
 def signup(request):
     if request.method == 'GET':
