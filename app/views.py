655a39a987f91bb330c10c420495188c246e7efd
alok gupta <galok8749@gmail.com>
2021-03-08 13:08:45 +0530

    signup page added

--- app/views.py
+++ app/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
+
+def home(request):
+    return render(request, 'app/index.html')
+
+def login(request):
+    return render(request, 'app/login.html')
+
+def signup(request):
+    if request.method == 'GET':
+        form = UserCreationForm()
+        context = {
+            'form': form
+        }
+        return render(request, 'app/signup.html', context=context)
+    else:
+        print(request.POST)
+        form = UserCreationForm(request.POST)
+        context = {
+            'form': form
+        }
+        if form.is_valid():
+            user = form.save()
+            print(user)
+            if user is not None:
+                return redirect('login')
+        else:
+            return render(request, 'app/signup.html', context=context)
